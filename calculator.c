d2f52c02e1117b74bccb28e75f28c56ef74d6fe8
vishnu <vishnutejavasam@gmail.com>
2024-10-21 21:05:37 +0530

    substraction implemented

--- calculator.c
+++ calculator.c
@@ -1,36 +1,10 @@
 #include <stdio.h>
-int add(int i, int j) {
- return (i + j);
+int sub(int i, int j) {
+ return (i - j);
 }
 int main() {
- int a, b;
- char operation;
- char confirm = 'n';
- do {
- printf("\nChoose an operation (+, -, *, /): ");
- scanf(" %c", &operation);
- printf("Enter first integer: ");
- scanf("%d", &a);
- printf("Enter second integer: ");
- scanf("%d", &b);
- switch (operation) {
- case '+':
- printf("The result of %d + %d is %d\n", a, b, add(a, b));
- break;
- case '-':
- printf("Will be implemented soon...\n");
- break;
- case '*':
- printf("Will be implemented soon...\n");
-break;
- case '/':
- printf("Will be implemented soon...\n");
- break;
- default:
- printf("Invalid operation. Please try again.\n");
- }
- printf("Do you want to perform another operation [y/N]? ");
- scanf(" %c", &confirm);
- } while (confirm == 'y' || confirm == 'Y');
- return 0;
+	int a, b;
+	scanf(" %d %d",&a,&b);
+	printf("%d",sub(a,b));
+	return 0;
 }
